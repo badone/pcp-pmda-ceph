490c6df70a472420a41b425226f666dc33545154
Brad Hubbard <bhubbard@redhat.com>
2016-07-07 11:22:52 +1000

    Implement interface to accept custom arguments and set up non-pmda "debug" mode.
    Signed-off-by: Brad Hubbard <bhubbard@redhat.com>

--- src/cephpmda.cpp
+++ src/cephpmda.cpp
@@ -31,3 +31,56 @@ pcp::pmda::fetch_value_result CephPmda::fetch_value(const metric_id & metric )
     return pcp::atom(metric.type,0);
 }
 
+boost::program_options::options_description CephPmda::get_supported_options() const
+{
+    using namespace boost::program_options;
+    options_description connectionOptions("Ceph connection options");
+    connectionOptions.add_options()
+        ("socket-dir", value<std::string>(), "Ceph admin socket directory");
+    return connectionOptions;
+}
+
+boost::program_options::options_description CephPmda::get_supported_hidden_options() const
+{
+    using namespace boost::program_options;
+    options_description options;
+    options.add_options()
+        ("no-pmda", bool_switch(), "run as a non-PMDA for development");
+    return options;
+}
+
+bool CephPmda::parse_command_line(const int argc, const char * const argv[],
+                                          pmdaInterface& interface,
+                                          boost::program_options::variables_map &options)
+{
+   // Let the parent implementation do the actually command line parsing.
+    if (!pcp::pmda::parse_command_line(argc, argv, interface, options)) {
+        return false; 
+    }
+
+    non_pmda_mode = ((options.count("no-pmda") > 0) && (options["no-pmda"].as<bool>()));
+    return true;
+}
+
+void CephPmda::initialize_pmda(pmdaInterface &interface)
+{
+    // If testing in non-PMDA mode, just wait for input then throw.
+    if (non_pmda_mode) {
+        std::cout << "Running in non-PMDA mode; outputting to: "
+                  << interface.version.two.ext->e_logfile << std::endl
+                  << "Press Enter to stop." << std::endl;
+        pmdaOpenLog(&interface);
+        std::getchar();
+        std::cout << "Stopping..." << std::endl;
+        #ifdef PM_ERR_FAULT // PM_ERR_FAULT added in PCP 3.6.0.
+        throw pcp::exception(PM_ERR_FAULT);
+        #else
+        throw pcp::exception(PM_ERR_GENERIC);
+        #endif
+    }
+
+    // Let the parent implementation initialize the rest of the PMDA.
+    pcp::pmda::initialize_pmda(interface);
+}
+
+
--- src/cephpmda.h
+++ src/cephpmda.h
@@ -17,19 +17,29 @@ class CephPmda: public pcp::pmda {
 public:
     CephPmda();
 
-    virtual std::string get_pmda_name() const;
+    virtual std::string get_pmda_name() const override;
 
-    virtual int get_default_pmda_domain_number() const;
+    virtual int get_default_pmda_domain_number() const override;
 
-    virtual std::string get_pmda_version() const;
-
-protected:
-
-    virtual pcp::metrics_description get_supported_metrics();
-
-    virtual pcp::pmda::fetch_value_result fetch_value(const metric_id &);
+    virtual std::string get_pmda_version() const override;
 
 protected:
 
+    virtual pcp::metrics_description get_supported_metrics() override;
+
+    virtual pcp::pmda::fetch_value_result fetch_value(const metric_id &) override;
+
+    virtual boost::program_options::options_description get_supported_options() const override;
+
+    virtual boost::program_options::options_description get_supported_hidden_options() const override;
+
+    virtual bool parse_command_line(const int argc, const char * const argv[],
+                                              pmdaInterface& interface,
+                                              boost::program_options::variables_map &options) override;
+
+    virtual void initialize_pmda(pmdaInterface &interface) override;
+
+private:
+    bool non_pmda_mode = false;
 };
 #endif
